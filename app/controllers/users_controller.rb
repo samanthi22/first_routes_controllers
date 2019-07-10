4f9a6eb58607675a96ef816842bc1d3cd3b6ce4e
Samanthi Jinadasa <sjinadasa@fas.harvard.edu>
2019-07-10 18:30:37 +0000

    Updated users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,34 @@
 class UsersController < ApplicationController
-    def index
-        render plain: "I'm in the index action!"
+     def index
+        users = User.all
+        self.render json: users
     end
+    
+     def create
+        user = User.new(name: params[:user].permit(:name, :email))
+        
+        if user.save!
+            render json: user
+        else 
+            render josn: user.errors.full_message, status: :unprocessable_entity
+        end
+    end
+    
+    def show
+        @user = User.find(params[:id])
+        self.render json: User.find(self.params[:id])
+    end
+    
+    def update
+        user = User.find(params[:id])
+        user.update(params[:user].permit(:name, :email))
+    end 
+    
+    def destroy
+       user = User.find(params[:id])
+       user.destroy
+       redirect_to users_url
+    end
+    
+    
 end
