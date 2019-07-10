355c6a986a4c67d6d021deffcab54c7789aebc7b
Samanthi Jinadasa <sjinadasa@fas.harvard.edu>
2019-07-10 17:57:19 +0000

    Routes working

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    resources :users
+    #resources :users
+    get 'users', to: 'users#index', as: 'users'
+    post 'users', to: 'users#create'
+    get 'users/new', to: 'users#new', as: "new_user"
+    patch 'users/:id', to: 'users#edit', as: "edit_user"
+    put 'users/:id', to: 'users#update'
+    delete 'users/:id', to: 'users#delete'
 end
